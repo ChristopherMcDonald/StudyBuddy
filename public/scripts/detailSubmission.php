a2e449ba3870bd66befa400a101a8c1413e88635
Christopher McDonald <mail@christophermcdonald.me>
2018-04-06 15:48:00 -0400

    small changes

--- public/scripts/detailSubmission.php
+++ public/scripts/detailSubmission.php
@@ -11,7 +11,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $id = $_SESSION["id"];
 if(!$id) {
-    $unencodedArray = ['resp' => 'invalid'];
+    $unencodedArray = ['resp' => 'invalid', 'reason' => 'not logged in'];
     echo json_encode($unencodedArray);
     return;
 }
@@ -31,7 +31,7 @@ if(strcmp($coffee,"yes") == 0) {
 }
 
 if(strlen($name) == 0 || strlen($address) == 0 || strlen($city) == 0 || strlen($prov) == 0 ) {
-    $unencodedArray = ['resp' => 'invalid'];
+    $unencodedArray = ['resp' => 'invalid', 'reason' => 'address not correct'];
     echo json_encode($unencodedArray);
     return;
 }
@@ -40,13 +40,13 @@ $postalExpression = '/^([a-zA-Z]\d[a-zA-Z])\ {0,1}(\d[a-zA-Z]\d)$/';
 $postalValid = (bool)preg_match($postalExpression, $postal);
 
 if(!$postalValid) {
-    $unencodedArray = ['resp' => 'invalid'];
+    $unencodedArray = ['resp' => 'invalid', 'reason' => 'postal not correct'];
     echo json_encode($unencodedArray);
     return;
 }
 
 if(!$comment) {
-    $unencodedArray = ['resp' => 'invalid'];
+    $unencodedArray = ['resp' => 'invalid', 'reason' => 'no review comment'];
     echo json_encode($unencodedArray);
     return;
 }
@@ -59,7 +59,6 @@ $lat = $obj->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
 $lng = $obj->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
 
 $rating =   $_POST["rating"];
-$files =    $_FILES['images'];
 
 $stmt = $conn->prepare("INSERT INTO Spaces (name, address, city, province, postal, lat, lng) VALUES (:name, :address, :city, :province, :postal, :lat, :lng)");
 $stmt->bindParam(':name', $name);
@@ -98,7 +97,7 @@ try {
     $file = $_FILES['images'];
     try {
 
-        if($file) {
+        if($_FILES['images']['size'] != 0) {
             $result = $s3Client->putObject([
                 'Bucket'     => $bucket,
                 'ContentType' => $file['type'],
@@ -114,9 +113,10 @@ try {
             $stmt->execute();
             $unencodedArray = ['resp' => 'valid', 'id' => $spaceId];
             echo json_encode($unencodedArray);
+        } else {
+            $unencodedArray = ['resp' => 'valid', 'id' => $spaceId];
+            echo json_encode($unencodedArray);
         }
-
-        return;
     } catch (Exception $e) {
         echo $e->getMessage() . "\n";
     }
